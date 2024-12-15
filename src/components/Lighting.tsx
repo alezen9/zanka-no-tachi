518f0a61113aecb603674b310b44d560c83e99a9
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2024-12-16 00:46:01 +0100

    feat: add proper lighting with transition to white when bankai is active

---
+++ src/components/Lighting.tsx
@@ -0,0 +1,98 @@
+import { AmbientLightProps, PointLightProps } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import useInterfaceStore from "../stores/useInterfaceStore";
+import { AmbientLight, PointLight } from "three";
+import gsap from "gsap";
+
+const Lighting = () => {
+  useEffect(() => {
+    const unsubscribe = useInterfaceStore.subscribe((state, prevState) => {
+      if (state.isBankaiActive !== prevState.isBankaiActive) {
+        // transition background color
+        document.body.classList.toggle("bankai");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <>
+      <CustomAmbientLight intensity={0} />
+      <CustomPointLight
+        color="darkorange"
+        position={[0, 10, -15]}
+        intensity={50}
+        decay={2}
+      />
+      <CustomPointLight
+        color="darkorange"
+        position={[-10, 10, 1]}
+        intensity={75}
+        decay={3}
+      />
+      <CustomPointLight
+        color="darkorange"
+        position={[10, 10, 1]}
+        intensity={75}
+        decay={3}
+      />
+    </>
+  );
+};
+
+export default Lighting;
+
+const CustomAmbientLight = (props: AmbientLightProps) => {
+  const { intensity } = props;
+  const light = useRef<AmbientLight>(null);
+  const gsapRef = useRef<GSAPTween>();
+
+  useEffect(() => {
+    const unsubscribe = useInterfaceStore.subscribe((state, prevState) => {
+      if (state.isBankaiActive !== prevState.isBankaiActive) {
+        gsapRef.current?.kill();
+        gsapRef.current = gsap.to(light.current, {
+          intensity: state.isBankaiActive ? 2 : intensity,
+          duration: state.isBankaiActive ? 0.25 : 0,
+          ease: "power2.out",
+          delay: state.isBankaiActive ? 0.5 : 0,
+        });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [intensity]);
+
+  return <ambientLight {...props} ref={light} />;
+};
+
+const CustomPointLight = (props: PointLightProps) => {
+  const { intensity } = props;
+  const light = useRef<PointLight>(null);
+  const gsapRef = useRef<GSAPTween>();
+
+  useEffect(() => {
+    const unsubscribe = useInterfaceStore.subscribe((state, prevState) => {
+      if (state.isBankaiActive !== prevState.isBankaiActive) {
+        gsapRef.current?.kill();
+        gsapRef.current = gsap.to(light.current, {
+          intensity: state.isBankaiActive ? 0 : intensity,
+          duration: state.isBankaiActive ? 0.25 : 0,
+          ease: "power2.out",
+          delay: state.isBankaiActive ? 0.5 : 0,
+        });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [intensity]);
+
+  return <pointLight {...props} ref={light} />;
+};
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,11 +1,13 @@
 import { GroupProps } from "@react-three/fiber";
 import GpgpuFire from "./GpgpuFire/GpgpuFire";
 import Yamamoto from "./Yamamoto";
+import Lighting from "./Lighting";
 
 const Scene = (props: GroupProps) => {
   return (
     <group {...props}>
       <TemporaryGround />
+      <Lighting />
       <Yamamoto />
       <GpgpuFire scale={[12, 20, 20]} position={[0, -2, 7.5]} />
     </group>
